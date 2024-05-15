9e8d6461de53f50f7cb6ae1b84178b983b044f9b
Jay <jeremyt89@ymail.com>
2024-05-15 12:02:56 -0400

    svg maker push deleted old branch had issues reuploaded complete work

---
+++ index.js
@@ -0,0 +1,94 @@
+//importing files
+const inquirer = require("inquirer");
+
+
+var fs = require("fs");
+
+//import shapes
+const { Triangle, Square, Circle } = require("./lib/shapes");
+
+
+function writeToFile(fileName, answers) {
+  
+  let svgString = "";
+  
+  svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  
+  svgString += "<g>";
+  
+  svgString += `${answers.shape}`;
+
+ // creating the shapes
+  let shape;
+  if (answers.shape === "Triangle") {
+    shape = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shape = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shape = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+
+  
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  
+  svgString += "</g>";
+  
+  svgString += "</svg>";
+
+  // write the file
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Created logo.svg");
+  });
+}
+
+// function to prompt the user for input
+function promptUser() {
+  inquirer
+    .prompt([
+      
+      {
+        type: "input",
+        message:
+          "What text would you like you logo to display? (Enter up to three characters)",
+        name: "text",
+      },
+      
+      {
+        type: "input",
+        message:
+          "Choose text color (Enter color keyword OR a hexadecimal number)",
+        name: "textColor",
+      },
+      
+      {
+        type: "list",
+        message: "What shape would you like the logo to render?",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      
+      {
+        type: "input",
+        message:
+          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+        name: "shapeBackgroundColor",
+      },
+    ])
+    .then((answers) => {
+      // if the user enters more than 3 characters, prompt the user again
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
+
+
+promptUser();
